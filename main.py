05f2882df0c239a51418396d99ae9b565bd783eb
Łukasz Majchrzycki <lukasz.majchrzycki@cdv.pl>
2024-12-10 13:29:49 +0100

    feat: add plot1

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ w = lambda x: (float( x.replace(" lbs.", "")) * lbs_to_kg )
 h = lambda x: (getFeet(x) * feet_to_m + getInch(x) * inch_to_m  )
 
 BMIdf = pd.read_csv('BMI.csv',  sep=";", converters={"Weight": w, "Height": h})
+
+BMIdf["BMI"] = BMIdf["Weight"]/BMIdf["Height"]**2
+BMI_categories = pd.DataFrame({
+    "name" : ["Underweight", "Healthy Weight", "Overweight", "Class 1 Obesity", "Class 2 Obesity", "Severe Obesity"],
+    "min": [0, 18.5, 25.0, 30.0, 35.0, 40.0],
+    "max": [18.5, 25.0, 30.0, 35.0, 40.0, 1000.0]
+})
+bins = [0] + BMI_categories["max"].tolist()
+labels = BMI_categories["name"].tolist()
+
+BMIdf["BMI_category"] = pd.cut(BMIdf["BMI"], bins=bins, labels=labels, right=False)
+
+BMIdf["BMI_category"].hist()
+plt.xticks(rotation=60)
+plt.show()
